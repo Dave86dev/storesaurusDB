06498084d4c84bfde215efe1de4f39e94c94226a
Dave86dev <davidochando1986@gmail.com>
2024-03-06 23:45:50 +0100

    feat: server separation done proficiently

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,17 @@
 import express from "express";
-import appConfig from "../config";
 import authRoutes from "./components/auth/public/authRoutes";
 import fileRoutes from "./components/file/public/fileRoutes";
-import pAuthRotues from "./components/auth/private/pAuthRoutes";
-import { initDbConnection } from "./db";
+import pAuthRoutes from "./components/auth/private/pAuthRoutes";
 import { handleError } from "./middlewares/handleError";
 
 const app = express();
-const PORT = appConfig.port;
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/file", fileRoutes);
+app.use("/admin/auth", pAuthRoutes);
 
-app.use("/admin/auth", pAuthRotues);
 app.use(handleError);
 
-const startServer = async () => {
-  try {
-    await initDbConnection();
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error starting the server:", error);
-  }
-};
-
-startServer();
+export default app;
